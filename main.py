48e31ed084c7dd09cf256c0221f923d8f1789236
George Lyu <georgemylyu@gmail.com>
2023-05-31 16:39:19 -0500

    Created Discord bot

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Hello world")
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(os.getenv('DISCORD_TOKEN'))
